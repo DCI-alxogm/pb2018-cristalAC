bb33febb5eaa328f57c555c76cf6d71ef3a24a20
cristalAC <42678969+cristalAC@users.noreply.github.com>
2018-08-30 11:36:01 -0500

    Update programa_hola.c

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -1,3 +1,22 @@
+*Esto es un comentario, sirve para escribir información relevante del programa que estoy haciendo, por ejemplo, este programa imprime la típica frase "hola mundo" en la pantalla*/
+
+	/*Programa creado por Cristal Araceli Cardoso Estrada*/
+	
+
+#include<stdio.h>
+int main (void)
+
+	{
+		
+		printf("¡Hola Cristal! :D\n");
+		printf("Este es el primer programa del curso de programación básica :D\n");
+	}
+
+
+/*Aquí termina nuestro programa*/
+
+
+
 /*Este es un comentario
 Programa creado el 06/08/2018 by Alma Gonzalez
 */
